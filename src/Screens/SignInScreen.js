87b9dc9e3fa5c023a38ce2936b65587213f88cb9
AQadir64 <a.qadir67@outlook.com>
2020-08-21 20:19:04 +0500

    initial setup files

---
+++ src/Screens/SignInScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const SignInScreen = () => {
+  return (
+    <View>
+      <Text>Sign In Screen</Text>
+    </View>
+  );
+};
+
+export default SignInScreen;
+
+const styles = StyleSheet.create({});
